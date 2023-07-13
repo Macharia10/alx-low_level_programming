f175642ff48b7e27bbdc13d111ef772e31aafca4
Macharia10 <sammy001sam@gmail.com>
2023-07-13 09:05:39 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main -  a program that prints a string using puts
+ * Return: 0 (Succes)
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
