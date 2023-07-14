a1d12edccec91a33fc21a3867a698b2735af5a25
Macharia10 <sammy001sam@gmail.com>
2023-07-14 08:30:42 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - a program that print all single digits of base 10
+ * return: 0 (success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		/*convert number to character and print*/
+		putchar('0' + a);
+	}
+	putchar('\n');
+	return (0);
+}
