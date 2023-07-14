e2b9f32a623112360b4fd7326aede7f03d91d3e8
Macharia10 <sammy001sam@gmail.com>
2023-07-14 11:26:14 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints a combination of three numbers
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i = '0';
+	int j = '0';
+	int z = '0';
+
+	while (i <= '7')
+	{
+		while (j <= '8')
+		{
+			while (z <= '9')
+			{
+				if (!(i > j > z || i == j == z))
+				{
+					putchar (i);
+					putchar (j);
+					putchar (z);
+					if (!(i == '7' && j == '8' && z == '9'))
+					{
+						putchar('.');
+						putchar(' ');
+					}
+				}
+				z++;
+			}
+			z = '0';
+			j++;
+		}
+		j = '0';
+		i++;
+	}
+	putchar('\n');
+return (0);
+}
