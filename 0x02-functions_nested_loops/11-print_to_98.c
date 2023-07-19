2bfa6679c3a411a4381793fe7c9b9d6f50eb4760
Macharia10 <sammy001sam@gmail.com>
2023-07-19 08:04:16 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,23 +13,15 @@ void print_to_98(int n)
 	{
 		for (; n <= 98; n++)
 		{
-			printf("%d", n);
-			if (n < 98)
-			{
-				printf(", ");
-			}
+			printf("%d, ", n);
 		}
 	}
 	else
 	{
 		for (; n > 98; n--)
 		{
-			printf("%d", n);
-			if (n > 98)
-			{
-				printf(", ");
-			}
+			printf("%d, ", n);
 		}
 	}
-	printf("%d", n);
+	printf("%d\n", n);
 }
