f394d45975fa847298d8ce4b04fd0538791963e8
Macharia10 <sammy001sam@gmail.com>
2023-08-17 06:13:48 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,52 +1,84 @@
 #include "variadic_functions.h"
-#include <stdio.h>
 #include <stdarg.h>
+#include <stdio.h>
 
 /**
- * print_all - Prints anything based on format specifiers.
- * @format: A string of format specifiers.
- *
- * @...: The variable arguments list.
+ * print_char - Prints a char
+ * @ap: Argument pointer
+ */
+void print_char(va_list ap)
+{
+	printf("%c", va_arg(ap, int));
+}
+
+/**
+ * print_integer - Prints an integer
+ * @ap: Argument pointer
+ */
+void print_integer(va_list ap)
+{
+	printf("%d", va_arg(ap, int));
+}
+
+/**
+ * print_float - Prints a float
+ * @ap: Argument pointer
+ */
+void print_float(va_list ap)
+{
+	printf("%f", va_arg(ap, double));
+}
+
+/**
+ * print_string - Prints a string
+ * @ap: Argument pointer
+ */
+void print_string(va_list ap)
+{
+	char *s = va_arg(ap, char *);
+
+	if (!s)
+	{
+		printf("(nil)");
+		return;
+	}
+		printf("%s", s);
+}
+
+/**
+ * print_all - Prints anything
+ * @format: Types of arguments passed to function
  */
 void print_all(const char * const format, ...)
 {
-	int i = 0, printed = 0;
-	char c, *s;
-	va_list args;
+	print_type types[] = {
+		{"c", print_char},
+		{"i", print_integer},
+		{"f", print_float},
+		{"s", print_string},
+		{NULL, NULL}
+	};
+	va_list ap;
+	char *separator = "";
+	int i = 0;
+	int j = 0;
 
-	va_start(args, format);
+	va_start(ap, format);
 	while (format && format[i])
 	{
-		switch (format[i])
+		while (types[j].type)
 		{
-			case 'c':
-				c = (char) va_arg(args, int);
-				printf("%c", c);
-				printed = 1;
-				break;
-			case 'i':
-				printf("%s%d", printed ? ", " : "", va_arg(args, int));
-				printed = 1;
-				break;
-			case 'f':
-				printf("%s%f", printed ? ", " : "", (float) va_arg(args, double));
-				printed = 1;
-				break;
-			case 's':
-				s = va_arg(args, char *);
-				if (s == NULL)
-					printf("%s(nil)", printed ? ", " : "");
-				else
-				{
-					printf("%s%s", printed ? ", " : "", s);
-					printed = 1;
-					break;
-				}
-				default:
-					break;
+			if (*types[j].type == format[i])
+			{
+				printf("%s", separator);
+				types[j].f(ap);
+				separator = ", ";
+			}
+			++j;
 		}
-		i++;
+		j = 0;
+		++i;
 	}
-	va_end(args);
 	printf("\n");
+	va_end(ap);
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,21 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+#include <stdarg.h>
+
+/**
+ * struct print - Struct print
+ *
+ * @type: The format specifier
+ * @f: The function associated
+ */
+typedef struct print_type
+{
+    char *type;
+    void (*f)(va_list ap);
+} print_type;
+
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
