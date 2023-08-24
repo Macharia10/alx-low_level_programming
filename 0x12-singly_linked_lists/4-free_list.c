9be0d9a33c9b44d7c3321f12e4ef4935e61bed10
Macharia10 <mash001sam@gmail.com>
2023-08-24 07:09:14 -0700

    4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -4,19 +4,18 @@
 * free_list - free a linked list
 *
 * @head: the head of the list to free
-* Return: void
 */
 void free_list(list_t *head)
 {
-	list_t *curr;
+	list_t *current;
 	list_t *next;
 
-	curr = head;
-	while (curr != NULL)
+	current = head;
+	while (current != NULL)
 	{
-		next = curr->next;
-		free(curr->str);
-		free(curr);
-		curr = next;
+		next = current->next;
+		free(current->str);
+		free(current);
+		current = next;
 	}
 }
