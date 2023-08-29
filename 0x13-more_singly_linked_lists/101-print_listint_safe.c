e443d12f65b6b0583c9f13f0d3a05969fde333b2
Macharia10 <mash001sam@gmail.com>
2023-08-29 00:34:12 -0700

    More singly linked lists

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,6 +1,31 @@
 #include "lists.h"
 
 /**
+ * find_listint_loop_l - find to loop a linked list
+ * @head: linked list to search
+ * Return: address of node where loop starts or returns, NULL if no loop
+ */
+listint_t *find_listint_loop_l(listint_t *head)
+{
+	listint_t *ptr, *end;
+
+	if (head == NULL)
+		return (NULL);
+
+	for (end = head->next; end != NULL; end = end->next)
+	{
+		if (end == end->next)
+			return (end);
+		for (ptr = head; ptr != end; ptr = ptr->next)
+		{
+			if (ptr == end->next)
+				return (end->next);
+		}
+	}
+	return (NULL);
+}
+
+/**
  * print_listint_safe - function that prints a listint_t linked list
  * @head: pointer to head node
  *
@@ -8,7 +33,7 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	listint_t *loop = find_listint_loop((listint_t *)head);
+	listint_t *loop = find_listint_loop_l((listint_t *)head);
 	size_t len = 0;
 
 	while (head && (head != loop || loop == NULL))
--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,6 +1,31 @@
 #include "lists.h"
 
 /**
+ * find_listint_loop_p - find a loop in linked list
+ * @head: list to search
+ * Return: address where the loop starts or returns, NULL if no loop
+ */
+listint_t *find_listint_loop_p(listint_t *head)
+{
+	listint_t *star, *end;
+
+	if (head == NULL)
+		return (NULL);
+
+	for (end = head->next; end != NULL; end = end->next)
+	{
+		if (end == end->next)
+			return (end);
+		for (star = head; star != end; star = star->next)
+		{
+			if (star == end->next)
+				return (end->next);
+		}
+	}
+	return (NULL);
+}
+
+/**
  * free_listint_safe - function that frees a listint_t list.
  * @h: pointer to pointer to head node
  *
@@ -9,7 +34,7 @@
 size_t free_listint_safe(listint_t **h)
 {
 	size_t s = 0;
-	listint_t *loopnode = find_listint_loop(*h);
+	listint_t *loopnode = find_listint_loop_p(*h);
 	listint_t *temp;
 
 	while (*h && ((*h != loopnode) || (loopnode == NULL)))
